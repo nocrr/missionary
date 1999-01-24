4aff30c0cc10ce9adec96024ccb44646040a66b3
Kemal Hadimli <disq@users.noreply.github.com>
1999-01-24 12:00:00 +0000

    Added lyrics.h on 1999-01-24

---
+++ lyrics.h
@@ -0,0 +1,109 @@
+char sira[42]={1,
+               1,
+               2,
+               2,
+               3,
+               3,
+               3,
+               4,
+               4,
+               4,
+               5,
+               5,
+               5,
+               6,
+               6,
+               6,
+               7,
+               7,
+               7,
+               8,
+               8,
+               8,
+               9,
+               9,
+               10,
+               10,
+               11,
+               11,
+               12,
+               12,
+               13,
+               14,
+               14,
+               15,
+               15,
+               16,
+               16,
+               17,
+               18,
+               18,
+               19,
+               19};
+
+int pospos[19]={2300,
+                2314,
+                2328,
+                2342,
+                2356,
+                2406,
+                2420,
+                2434,
+                2448,
+                2462,
+                2513,
+                2527,
+                2541,
+                2555,
+                2606,
+                2620,
+                2634,
+                2648,
+                2662};
+
+typedef char siirtyp[42][21];
+
+//             1234567890123456789012345678901234567890
+siirtyp siir={"  Ademden ogrendik  \0",//1
+              "     savasmayi,     \0",//2
+              "     yumruklari     \0",//3
+              "   o ogretti bize   \0",//4
+              "Savasarak yaslandik,\0",//5
+              "    gencler aldi    \0",//6
+              "      yerimizi.     \0",//7
+              " Baris dolu umutlar \0",//8
+              "     ve yepyeni     \0",//9
+              "    silahlar ile.   \0",//10
+              "     Ufacik bir     \0",//2
+              " kivilcimla basladi \0",//3
+              "       yangin...    \0",//4
+              "  Gokyuzu karardi,  \0",//5
+              "   toprak isindi    \0",//6
+              "  cehennem misali.  \0",//7
+              "  Yuruduk uzerine   \0",//8
+              "     dusmanin,      \0",// 9
+              "      ezerek,       \0", //20
+              "     Yok ederek     \0",//1
+              "   binlerce yilin   \0",//2
+              "      emegini.      \0",//3
+              "     Carpistik      \0",//4
+              "   gogus  goguse,   \0",//5
+              "      Savastik      \0",//6
+              "  Aslanlar gibi...  \0",//7
+              "Dusunmedik insanligi\0",//8
+              "    unuttuk onu.    \0",//9
+              "Kazandik ayni oyunu \0",//31
+              "    defalarca...    \0",//3
+              "Kaybederek kendimizi\0",//4
+              "  Kan ile kutladik  \0",//5
+              "    zaferimizi..    \0",//6
+              "    Yaralarimizi    \0",//7
+              "     sararken..     \0",//8
+              "Biliyorum ki  simdi;\0",//40
+              "Insanoglu ogrenemez.\0",//1
+              "Ogrenemez insanligi.\0",//2
+              "Agliyorum yas yerine\0",//3
+              "     kan ile...     \0",//4
+              "   Mezari basinda   \0",//5
+              "     insanligin     \0"};//7
+
